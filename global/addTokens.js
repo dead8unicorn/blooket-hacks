2bd8aa8100b163210f92f058c336542938df93b1
dead8unicorn <113631040+dead8unicorn@users.noreply.github.com>
2022-09-15 13:51:11 -0400

    1000

--- global/addTokens.js
+++ global/addTokens.js
@@ -50,7 +50,7 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
+                            addedTokens: 1000,
                             addedXp: 300
                         }, e.secret)
                     }).then(() => alert('Added daily rewards!')).catch(() => alert('There was an error when adding rewards!'));;
